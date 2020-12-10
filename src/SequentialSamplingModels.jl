73984505e1e0779aa93c977d4b04c7530dfb1d70
itsdfish <itsdfish@gmail.com>
2020-12-10 10:51:49 -0500

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.13

---
+++ src/SequentialSamplingModels.jl
@@ -0,0 +1,5 @@
+module SequentialSamplingModels
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using SequentialSamplingModels
+using Test
+
+@testset "SequentialSamplingModels.jl" begin
+    # Write your tests here.
+end
