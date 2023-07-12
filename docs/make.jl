80d55746eab2bd07bde8f0476d14a0d6602f9966
Dominique Makowski <dom.mak19@gmail.com>
2023-07-12 08:43:15 +0100

    minor docs formatting

--- docs/make.jl
+++ docs/make.jl
@@ -2,35 +2,39 @@ using Documenter
 using SequentialSamplingModels
 
 makedocs(
-    sitename = "SequentialSamplingModels",
-    format = Documenter.HTML(
-        assets = [
+    sitename="SequentialSamplingModels",
+    format=Documenter.HTML(
+        assets=[
             asset(
                 "https://fonts.googleapis.com/css?family=Montserrat|Source+Code+Pro&display=swap",
-                class = :css,
+                class=:css,
             ),
         ],
-        collapselevel = 1,
+        collapselevel=1,
     ),
-    modules = [SequentialSamplingModels],
-    pages = ["Home" => "index.md",
-            "API" => "api.md",
-            "Models" => ["Attentional Drift Diffusion" => "aDDM.md",
-                "Drift Diffusion Model" => "DDM.md",
-                "Leaky Competing Accumulator" => "lca.md",
-                "Linear Ballistic Accumulator" => "lba.md",
-                "Lognormal Race Model" => "lnr.md",
-                "Muti-attribute attentional drift diffusion Model" => "maaDDM.md",
-                "Racing Diffusion Model" => "rdm.md",
-                "Wald Model" => "wald.md",
-                "Wald Mixture Model" => "wald_mixture.md"],
-            "Parameter Estimation" => ["Simple Bayesian" => "turing_simple.md",
+    modules=[SequentialSamplingModels],
+    pages=[
+        "Home" => "index.md",
+        "Models" => [
+            "Drift Diffusion Model (DDM)" => "DDM.md",
+            "Linear Ballistic Accumulator (LBA)" => "lba.md",
+            "Lognormal Race Model (LNR)" => "lnr.md",
+            "Attentional Drift Diffusion" => "aDDM.md",
+            "Leaky Competing Accumulator" => "lca.md",
+            "Muti-attribute attentional drift diffusion Model" => "maaDDM.md",
+            "Racing Diffusion Model" => "rdm.md",
+            "Wald Model" => "wald.md",
+            "Wald Mixture Model" => "wald_mixture.md"
+        ],
+        "Parameter Estimation" => [
+            "Simple Bayesian" => "turing_simple.md",
             "Advanced Bayesian" => "turing_advanced.md",
-            ],
-            "Developer Guide" => "developer_guide.md"
-            ]
+        ],
+        "API" => "api.md",
+        "Developer Guide" => "developer_guide.md"
+    ]
 )
 
 deploydocs(
-    repo = "github.com/itsdfish/SequentialSamplingModels.jl.git",
+    repo="github.com/itsdfish/SequentialSamplingModels.jl.git",
 )
