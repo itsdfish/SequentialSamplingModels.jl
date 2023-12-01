78326a4955275a6b6172749377fe9ab7660337f0
itsdfish <itsdfish@gmail.com>
2023-12-01 08:24:53 -0500

    fix printing for PrettyTables 2.0

--- src/utilities.jl
+++ src/utilities.jl
@@ -18,11 +18,11 @@ function _show(io::IO, model)
     return pretty_table(io,
         values;
         title=model_name,
-        row_name_column_title="Parameter",
+        row_label_column_title="Parameter",
         compact_printing=false,
         header=["Value"],
-        row_name_alignment=:l,
-        row_names=[fieldnames(typeof(model))...],
+        row_label_alignment=:l,
+        row_labels=[fieldnames(typeof(model))...],
         formatters=ft_printf("%5.2f"),
         alignment=:l,
     )
